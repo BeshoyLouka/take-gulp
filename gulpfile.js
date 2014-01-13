c83f5df39f2277493d32a676890d89510e187919
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-13 13:08:52 -0500

    gulp-browserifying all scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+    //single entry point to browserify
+    gulp.src(['public/js/app.js'])
+        .pipe(browserify({
+          insertGlobals : true,
+          debug : true
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./public/js'))
+});
 
 gulp.task('default', function(){
   // place code for your default task here
