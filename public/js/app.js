17e1a7cbebae234cc43837233817fd2f552e4b55
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-13 14:58:46 -0500

    trying to require bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var Backbone = require("backbone");
 var $ = require('jquery');
-var bootstrap = require('./bootstrapJs');
+var jQuery = require('twitter-bootstrap-3.0.0/dist/js/bootstrap');
 
 
 Backbone.$ = $;
