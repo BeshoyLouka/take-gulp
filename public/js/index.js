e65c39c3e757e5b8149193d91a7f56246fc65276
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-13 12:54:18 -0500

    created a native server in node

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+var http = require("http");
+var fs = require("fs");
+console.log("Starting");
+
+var config = JSON.parse(fs.readFileSync("config.json"));
+var host = config.host;
+var port = config.port;
+
+var server = http.createServer(function(request, response){
+    console.log("Received request: " + request.url);
+    fs.readFile("../templates" + request.url, function(error, data){
+        if (error) {
+            response.writeHead(404, {"Content-type":"text/plain"});
+            response.end("Sorry the page was not found");
+        } else {
+            response.writeHead(200, {"Content-type":"text/html"});
+            response.end(data);
+        }
+    });
+});
+
+server.listen(port, host, function(){
+    console.log("Listening " + host + ":" + port);
+});
+
+fs.watchFile("config.json", function(){
+    config = JSON.parse(fs.readFileSync("config.json"));
+    host = config.host;
+    port = config.port;
+    server.close();
+    server.listen(port, host, function(){
+        console.log("Now listening " + host + ":" + port);
+    }); 
+});
+
